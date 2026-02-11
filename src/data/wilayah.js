5f9d218442b7ba2cc4829af47ea21dec34cf847b
Be Rech <berech@Bes-MacBook-Air.local>
2026-02-11 23:06:35 +0800

    Sort address data alphabetically

--- src/data/wilayah.js
+++ src/data/wilayah.js
@@ -1,21 +1,25 @@
 const BASE_URL = 'https://emsifa.github.io/api-wilayah-indonesia/api';
 
 export async function getProvinces() {
-    const res = await fetch(`${BASE_URL}/provinces.json`);
-    return res.json();
+    const response = await fetch(`${BASE_URL}/provinces.json`);
+    const data = await response.json();
+    return data.sort((a, b) => a.name.localeCompare(b.name));
 }
 
 export async function getRegencies(provinceId) {
-    const res = await fetch(`${BASE_URL}/regencies/${provinceId}.json`);
-    return res.json();
+    const response = await fetch(`${BASE_URL}/regencies/${provinceId}.json`);
+    const data = await response.json();
+    return data.sort((a, b) => a.name.localeCompare(b.name));
 }
 
 export async function getDistricts(regencyId) {
-    const res = await fetch(`${BASE_URL}/districts/${regencyId}.json`);
-    return res.json();
+    const response = await fetch(`${BASE_URL}/districts/${regencyId}.json`);
+    const data = await response.json();
+    return data.sort((a, b) => a.name.localeCompare(b.name));
 }
 
 export async function getVillages(districtId) {
-    const res = await fetch(`${BASE_URL}/villages/${districtId}.json`);
-    return res.json();
+    const response = await fetch(`${BASE_URL}/villages/${districtId}.json`);
+    const data = await response.json();
+    return data.sort((a, b) => a.name.localeCompare(b.name));
 }
